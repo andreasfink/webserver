3c8352139ecc8ad4ac6a06c62f4f272bbc468416
Andreas Fink <andreas@fink.org>
2016-10-27 12:58:14 +0200

    make it work only with Foundation

--- webserver/main.m
+++ webserver/main.m
@@ -6,9 +6,42 @@
 //  Copyright (c) 2013 Andreas Fink. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <ulib/ulib.h>
+#import "AppDelegate.h"
+
+time_t g_startup_time = 0;
+BOOL must_quit = NO;
+AppDelegate *g_app_delegate = NULL;
 
 int main(int argc, const char * argv[])
 {
-    return NSApplicationMain(argc, argv);
+    time_t	tim;
+    char	state_array[16];
+    NSRunLoop *runLoop;
+
+    [NSUserDefaults standardUserDefaults];
+
+    @autoreleasepool
+    {
+        tim = time(&g_startup_time);
+        initstate((unsigned  int)tim,  state_array,  16);
+
+        runLoop = [NSRunLoop currentRunLoop];
+        g_app_delegate = [[AppDelegate alloc]init];
+        [NSOperationQueue mainQueue];
+        NSNotification *notif = [NSNotification notificationWithName:@"dummy" object:NULL];
+        [g_app_delegate applicationDidFinishLaunching:notif];
+    }
+    while(must_quit==0)
+    {
+        @autoreleasepool
+        {
+            [runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+        }
+    }
+    @autoreleasepool
+    {
+        NSLog(@"******************* SYSTEM TERMINATING *******************");
+    }
+    return 0;
 }
